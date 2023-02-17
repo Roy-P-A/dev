301f91baa032097832c986d32532b8155c81c896
Roy P A <roy@nintrivalabs.com>
2023-02-17 12:42:27 +0530

    test1234

---
+++ src/main/java/com/spring/devo/DevoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.devo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoApplication.class, args);
+	}
+
+}
